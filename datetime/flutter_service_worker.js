8a0c02f87e006db9cdde4543cc5f20a9ce38cabb
prakhar <prakhar@bu.edu>
2023-01-24 12:45:50 -0500

    update fix

--- datetime/flutter_service_worker.js
+++ datetime/flutter_service_worker.js
@@ -17,9 +17,9 @@ const RESOURCES = {
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "c363ae02a17afb7072250414b09eed89",
-"/": "c363ae02a17afb7072250414b09eed89",
-"main.dart.js": "f2eff9e6096e6d012d43efce556f97ab",
+"index.html": "13c3f22145fcf1f4eb2745b8ad4b5966",
+"/": "13c3f22145fcf1f4eb2745b8ad4b5966",
+"main.dart.js": "97fa4ff56632b5c98412e972c7298563",
 "manifest.json": "15f73b7e8a8209c2206210b3ac8dea1b",
 "version.json": "ff966ab969ba381b900e61629bfb9789"
 };
