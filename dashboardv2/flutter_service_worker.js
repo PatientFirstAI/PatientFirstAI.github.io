178c5326ada049a51b8dc06ea396705d8c1f7030
prakhar <prakhar@bu.edu>
2023-03-06 17:15:23 -0500

    update fixed

--- dashboardv2/flutter_service_worker.js
+++ dashboardv2/flutter_service_worker.js
@@ -642,9 +642,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "fd310dcfad46c82852e6dc20d9dfd25e",
-"/": "fd310dcfad46c82852e6dc20d9dfd25e",
-"main.dart.js": "b759cb952edb3eb9f496125002058e0c",
+"index.html": "903a29d0fd84d7808ebc9bb76a33987b",
+"/": "903a29d0fd84d7808ebc9bb76a33987b",
+"main.dart.js": "098a859cc31ec388af7bb3678d52b98c",
 "manifest.json": "f31d349ba9cc7d6004a97996cecc1c27",
 "version.json": "a97c5308d752c1a7fba9cb997ec81fcb"
 };
